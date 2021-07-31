a6ef4a9ac2659d2b81edf993e85225bf3b357ac1
Sandas24 <88206656+Sandas24@users.noreply.github.com>
2021-07-31 21:41:16 +0100

    Add files via upload

---
+++ coding excersise 2.py
@@ -0,0 +1,9 @@
+def func_cal(num,denom):
+  userInput1=input("Enter denominator:")
+  userInput2=input("Enter numerator:")
+
+if userInput1==0:
+   print("Error-You cannot divide by 0.Please choose appropriate denominator")
+else:
+   print(userInput2/userInput1)
+func_cal(1/2)
